f0554a5bd8daa6ab3197c2ad386c29f53bd29411
Kian <kishanbarmawala.sculptsoft@gmail.com>
2022-04-27 12:59:06 +0530

    - UI changes made

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, FlatList, SafeAreaView, Text } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { FlatList, Image, SafeAreaView, Text, View } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import style from './styles';
 
 const Home = () => {
     const [formData, setFormData] = useState([]);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordVisible, setPasswordVisible] = useState(true);
+
+    [
+        {
+            name: '',
+            email: '',
+            pass: '',
+        },
+        {
+            name: '',
+            email: '',
+            pass: '',
+        },
+    ]
 
     _onButtonPress = (type) => {
         if (type === 0) {
@@ -12,16 +30,62 @@ const Home = () => {
             temp.splice(formData.length - 1, 1);
             setFormData(temp);
         } else {
-            setFormData([...formData, randomRgb()]);
+            let data = {
+                name: name,
+                email: email,
+                pass: password,
+            }
+            setFormData([...formData, data]);
+            setName('');
+            setEmail('');
+            setPassword('');
+            setPasswordVisible('');
         }
     }
-    
+    onPressEyeButton = () => {
+        console.log("onPressEyeButton");
+        setPasswordVisible(!passwordVisible);
+    }
+
+    const _onSubmitPress = () => {
+        if (name.length === 0) {
+            alert("Please enter name")
+        } else if (name.length < 5) {
+            alert("Name should be 5 characters long")
+        } else if (email.length === 0) {
+            alert("Please enter email")
+        } else if (emailValidate(email) === false) {
+            alert("Enter correct email")
+        } else if (password.length === 0) {
+            alert("Please enter password")
+        } else if (passValidate(password) === false) {
+            alert("Use minimum eight characters, at least one letter and one number for password")
+        } else {
+            alert("You're good to go")
+        }
+    }
+
+    const emailValidate = (text) => {
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+        if (reg.test(text) === false) {
+            return false
+        }
+        return true
+    }
+
+    const passValidate = (text) => {
+        // let reg = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+        let reg = /^(?!.*[\s])(?=.*\d)(?=.*[!@#$%^&_*+])(?=.*[a-z])(?=.*[A-Z]).{8,}$/
+        if (reg.test(text) === false) {
+            return false
+        }
+        return true
+    }
+
     return (
         <SafeAreaView>
             <View style={ style.headerView }>
-                <View style={ style.headerButtonView }>
-                    <Text style={ style.headerText }>User From</Text>
-                </View>
+                <Text style={ { fontSize: 20 } }>{ 'Employees Details' }</Text>
                 <View style={ style.headerButtonView }>
                     <TouchableOpacity style={ style.headerButtonText } onPress={ () => _onButtonPress(0) }>
                         <Text style={ style.buttonText } >{ '-' }</Text>
@@ -34,60 +98,45 @@ const Home = () => {
             <FlatList
                 keyExtractor={ item => item }
                 data={ formData }
-                renderItem={ ({ item }) => {
-                    return <View style={ [{ backgroundColor: item }, style.colorView] } />;
+                renderItem={ ({ item, index }) => {
+                    return <View style={ [/*{ backgroundColor: item },*/ style.colorView] }>
+                        <Text style={ style.formTitle }>{ 'Employee details form' }</Text>
+                        <TextInput
+                            style={ style.textInput }
+                            placeholder="Enter name"
+                            value={ name }
+                            onChangeText={ text => setName(text) }
+                        />
+                        <TextInput
+                            style={ style.textInput }
+                            placeholder="Enter email"
+                            keyboardType='email-address'
+                            value={ email }
+                            onChangeText={ text => setEmail(text) }
+                        />
+                        <View style={ style.textInput }>
+                            <TextInput
+                                placeholder="Enter password"
+                                secureTextEntry={ passwordVisible }
+                                value={ password }
+                                style={ { width: '86%', height: 30 } }
+                                onChangeText={ text => setPassword(text) }
+                            />
+                            <TouchableOpacity style={ { height: 35, width: 35, justifyContent: 'center', alignItems: 'center' } } activeOpacity={ .5 } onPress={ () => onPressEyeButton() }>
+                                <Image style={ style.imageIcon } source={ passwordVisible ? require('../../icons/eye.png') : require('../../icons/eye-slash.png') } />
+                            </TouchableOpacity>
+                        </View>
+                        <View style={ { alignItems: 'center' } }>
+                            <TouchableOpacity style={ style.buttonStyle } onPress={ () => _onSubmitPress() }>
+                                <Text style={ { fontSize: 18 } }>{ 'Submit' }</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                 } }
             />
         </SafeAreaView>
     )
+
 }
-
-const randomRgb = () => {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-    return `rgb(${red},${green},${blue})`;
-};
-
-const style = StyleSheet.create({
-    headerView: {
-        justifyContent: 'space-between',
-        height: 44,
-        backgroundColor: 'rgba(52, 52, 52, 0.1)',
-        flexDirection: 'row',
-        paddingHorizontal: 16,
-        alignItems: 'center',
-    },
-    headerButtonView: {
-        height: 44,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    headerText: {
-        width: 44,
-        height: 44,
-        justifyContent: 'center',
-        fontSize: 20,
-        alignItems: 'center',
-    },
-    headerButtonText: {
-        width: 44,
-        height: 44,
-        justifyContent: 'center',
-        backgroundColor: 'yellow',
-        fontSize: 20,
-        alignItems: 'center',
-        backgroundColor: 'rgba(52, 52, 52, 0.1)',
-        marginLeft: 8
-    },
-    colorView: {
-        width: 100,
-        height: 100,
-    },
-    buttonText: {
-        fontSize: 26
-    },
-});
 
 export default Home;
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,73 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    headerView: {
+        justifyContent: 'space-between',
+        height: 44,
+        backgroundColor: 'rgba(52, 52, 52, 0.1)',
+        flexDirection: 'row',
+        paddingHorizontal: 16,
+        alignItems: 'center',
+    },
+    headerButtonView: {
+        height: 44,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerButtonText: {
+        width: 34,
+        height: 34,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(52, 52, 52, 0.05)',
+        borderRadius: 20,
+        marginLeft: 12
+    },
+    colorView: {
+        marginVertical: 8,
+        marginHorizontal: 8,
+        padding: 4,
+        borderRadius: 10,
+        backgroundColor: 'rgba(52, 52, 52, 0.05)'
+    },
+    buttonText: {
+        fontSize: 30,
+    },
+    textInput: {
+        height: 40,
+        marginLeft: 12,
+        marginRight: 12,
+        marginBottom: 12,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 4,
+        borderColor: 'rgba(52, 52, 52, 0.5)',
+        justifyContent: 'space-between', 
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    formTitle: {
+        fontSize: 16,
+        height: 40,
+        marginTop: 12,
+        marginLeft: 14
+    },
+    statusBarStyle: {
+        // flex: 1,
+        backgroundColor: "rgba(52, 52, 52, 0.1)",
+    },
+    buttonStyle: {
+        marginVertical: 16,
+        width: 120,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(52, 52, 52, 0.1)',
+        borderRadius: 8,
+    },
+    imageIcon: {
+        width: 22,
+        height: 22
+    }
+});
